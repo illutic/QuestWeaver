b9ca2728c8af3dc62cfd766e039f45b35ef130d7
George Sigalas <georgesg97@gmail.com>
2024-03-31 12:12:33 +0300

    fix remote config fetch and activation

--- app/src/main/java/g/sig/questweaver/remoteconfig/RemoteConfigRepositoryImpl.kt
+++ app/src/main/java/g/sig/questweaver/remoteconfig/RemoteConfigRepositoryImpl.kt
@@ -6,53 +6,35 @@ import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import g.sig.domain.repositories.RemoteConfigRepository
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.retryWhen
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 import kotlin.math.pow
 import kotlin.math.roundToLong
 import kotlin.time.Duration.Companion.hours
 
 class RemoteConfigRepositoryImpl : RemoteConfigRepository {
-    private inner class FetchRemoteConfigException : Exception()
-
-    private val fetchAndActivateFlow = callbackFlow {
+    private suspend fun fetchAndActivateRemoteConfig() = suspendCancellableCoroutine { continuation ->
         val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
         val configSettings = remoteConfigSettings {
             minimumFetchIntervalInSeconds = if (BuildConfig.DEBUG) 3600 else 12.hours.inWholeSeconds
         }
         remoteConfig.setConfigSettingsAsync(configSettings)
         remoteConfig.fetchAndActivate().addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                launch {
-                    send(true)
-                    close()
-                }
-            } else {
-                launch {
-                    send(false)
-                    close(FetchRemoteConfigException())
-                }
-            }
+            continuation.resume(task.isSuccessful)
         }
-        awaitClose()
     }
 
-    private fun exponentialBackoff(attempt: Long, exponent: Double = 2.0): Long =
-        exponent.pow(attempt.toInt()).roundToLong() * 1000
+    private fun exponentialBackoff(attempt: Int, exponent: Double = 2.0): Long =
+        exponent.pow(attempt).roundToLong() * 1000
 
     override suspend fun fetchRemoteConfig() {
-        fetchAndActivateFlow.retryWhen { cause, attempt ->
-            if (cause is FetchRemoteConfigException) {
-                delay(exponentialBackoff(attempt))
-                true
-            } else {
-                false
-            }
-        }.first()
+        var retryCount = 0
+        do {
+            val activatedRemoteConfig = fetchAndActivateRemoteConfig()
+            delay(exponentialBackoff(retryCount))
+            retryCount++
+        } while (!activatedRemoteConfig)
     }
 
     override suspend fun getRemoteConfigValue(key: String): String =
